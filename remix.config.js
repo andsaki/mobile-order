ae40e53d6e21487eddda2ec262ff45f0f1c9690a
andsaki <ruby.on.rails.api@gmail.com>
2025-05-29 14:15:09 +0900

    feat: remix.config.js

---
+++ remix.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  // GitHub Pages用の設定
+  publicPath: "/mobile-order/",
+  // 静的サイト生成用の設定
+  future: {
+    v2_routeConvention: true,
+  },
+  // GitHub Pages用の静的サイト生成
+  serverBuildPath: "build/index.js",
+};
