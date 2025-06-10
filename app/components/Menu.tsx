1ddcfe49df4cc911599ec34e11f37877c4ed1817
andsaki <ruby.on.rails.api@gmail.com>
2025-06-10 14:28:34 +0900

    feat: API連動

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -4,17 +4,29 @@ import { FaWineBottle, FaHamburger } from "react-icons/fa";
 
 interface MenuItem {
   id: string;
-  categoryId: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  categoryId: number;
   name: string;
   price: number;
   description: string;
-  image: string;
+  image: {
+    url: string;
+    height: number;
+    width: number;
+  };
 }
 
 interface Category {
   id: string;
+  categoryId: number;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
   name: string;
-  categoryId: string;
 }
 
 interface MenuData {
@@ -23,26 +35,20 @@ interface MenuData {
 }
 
 interface MenuProps {
-  menuData: MenuData;
+  menuData?: MenuData;
 }
 
 const Menu: React.FC<MenuProps> = ({ menuData }) => {
   return (
     <div className="container mx-auto py-4">
-      {menuData.categories.map((category) => (
+      {menuData?.categories.map((category) => (
         <div key={category.id} className="mb-8">
           <h2 className="text-2xl mb-4 font-bold flex items-center">
             {category.name}
-            {category.categoryId === "2" && (
-              <FaWineBottle className="ml-2 mb-1" />
-            )}
-            {category.categoryId === "3" && (
-              <FaHamburger className="ml-2 mb-1" />
-            )}
           </h2>
           <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {menuData.items
-              .filter((item) => item.categoryId === category.id)
+              .filter((item) => item.categoryId === category.categoryId)
               .map((item) => (
                 <li
                   key={item.id}
@@ -50,7 +56,7 @@ const Menu: React.FC<MenuProps> = ({ menuData }) => {
                 >
                   <Link to={`/menu/${item.id}`} className="flex">
                     <img
-                      src={item.image}
+                      src={item.image.url}
                       alt={item.name}
                       className="w-32 h-32 object-cover rounded-md mr-4"
                     />
@@ -69,4 +75,5 @@ const Menu: React.FC<MenuProps> = ({ menuData }) => {
   );
 };
 
+export type { MenuItem, Category };
 export default Menu;
