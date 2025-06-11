d466353ff0b431d233622dff4fe174c2fe6e8649
andsaki <ruby.on.rails.api@gmail.com>
2025-06-11 17:44:44 +0900

    fix: css左右の余白を追加

--- app/routes/menu/_index.tsx
+++ app/routes/menu/_index.tsx
@@ -52,7 +52,7 @@ export default function MenuRoute() {
   const menu = useLoaderData<typeof loader>();
   console.log("menu:", menu);
   return (
-    <div className="container mx-auto py-4">
+    <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">メニュー</h1>
       <Menu menuData={menu} />
     </div>
