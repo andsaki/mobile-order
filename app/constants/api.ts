9c7d4e3fd384a9722256bdeb2614cec0ec759c63
andsaki <ruby.on.rails.api@gmail.com>
2025-06-24 12:58:00 +0900

    feat: 定数ファイルからimport

---
+++ app/constants/api.ts
@@ -0,0 +1,5 @@
+// APIエンドポイントの定数。microCMSのアイテムAPIにアクセスするためのURLを定義します。
+export const API_ENDPOINT = "https://andsakiapi.microcms.io/api/v1/items";
+
+// APIキー。microCMS APIへの認証に使用されるキーです。セキュリティ上の理由から、環境変数として管理することが推奨されます。
+export const API_KEY = "KOjYGzOL5TlpVlL8YAZdxka6KEPLlDaBtPW2";
