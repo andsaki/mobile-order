b957e264e655aecc61d367cb3da6107d546823dd
andsaki <ruby.on.rails.api@gmail.com>
2025-06-24 14:10:40 +0900

    Remove typeErrorTest.ts to resolve type error in CI

--- app/test/typeErrorTest.ts
+++
@@ -1,8 +0,0 @@
-import { CartItem } from "../types/cartItem";
-
-const item: CartItem = {
-  id: "1",
-  name: "Test Item",
-  price: "100", // Intentional type error: price should be a number, not a string
-  quantity: 1,
-};
