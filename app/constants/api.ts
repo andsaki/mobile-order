28826a6d1ad42287c5b5deca3785bdecc3c05e8e
andsaki <ruby.on.rails.api@gmail.com>
2025-08-07 15:49:15 +0900

    fix: Resolve typecheck error by adding API_DELETE_ORDERS

--- app/constants/api.ts
+++ app/constants/api.ts
@@ -2,3 +2,4 @@
 export const API_ENDPOINT = "https://andsakiapi.microcms.io/api/v1/items";
 export const API_ORDER_ROUTE = "/api/order";
 export const CATEGORIES_API_ENDPOINT = "https://andsakiapi.microcms.io/api/v1/categories";
+export const API_DELETE_ORDERS = "/api/delete-orders";
