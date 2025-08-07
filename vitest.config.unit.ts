a8101b024f2bcacf7de1cef9ebea816472cb45f7
andsaki <ruby.on.rails.api@gmail.com>
2025-08-07 15:56:44 +0900

    fix: Set dummy MICROCMS_API_KEY for unit tests in CI

--- vitest.config.unit.ts
+++ vitest.config.unit.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  define: { 'process.env.MICROCMS_API_KEY': JSON.stringify('dummy_api_key') },
   test: {
     globals: true,
     environment: "node", // 単体テストなのでnode環境
